e400831c541b0d96f0defe3e07f6b2fe276bf4c8
andresperezfc <felipe3apc@gmail.com>
2023-01-18 20:21:53 -0500

    useState

--- src/index.js
+++ src/index.js
@@ -5,76 +5,59 @@ import { Boton } from "./Boton";
 import { TaskCard } from "./Task";
 import { Saludar } from "./Saludar";
 import { Posts } from "./Post";
+
+import { useState } from "react";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const users = [
-  {
-    id: 1,
-    image: "https://robohash.org/user",
-    name: "andres",
-  },
-  {
-    id: 2,
-    image: "https://robohash.org/user2",
-    name: "Felipe",
-  },
-  {
-    id: 3,
-    image: "https://robohash.org/user3",
-    name: "Pérez",
-  },
-  {
-    id: 4,
-    image: "https://robohash.org/user4",
-    name: "Campo",
-  },
-];
+let counter = 10;
 
-function Saludos({ title, name = "user" }) {
-  //const name = "Andrés";
-  console.log("");
+function Counter() {
+  const [counter, setCounter] = useState(0);
 
+  const [mensaje, setMensaje] = useState("");
   return (
-    <h1>
-      {title} {name}
-    </h1>
+    <div>
+      <h1>Counter: {counter}</h1>
+      <button
+        onClick={() => {
+          setCounter(counter + 10);
+        }}
+      >
+        Sumar
+      </button>
+
+      <button
+        onClick={() => {
+          setCounter(counter - 10);
+        }}
+      >
+        Restar
+      </button>
+
+      <button
+        onClick={() => {
+          setCounter(0);
+        }}
+      >
+        Reiniciar
+      </button>
+      <br />
+      <br />
+      <input onChange={(e) => setMensaje(e.target.value)}></input>
+      <button
+        onClick={() => {
+          alert("El mensaje es: " + mensaje);
+        }}
+      >
+        Guardar
+      </button>
+    </div>
   );
 }
 
 root.render(
   <>
-    <Boton text="Click me" />
-    <Boton text="Pay me" />
-    <Boton
-      text="Texto añadido para solucionar el error
-    "
-    />
-
-    <Saludos title="Hola React" />
-    <NuevoMundo></NuevoMundo>
-    <UserCard
-      name="Andres Perez"
-      amount={120000}
-      married={true}
-      points={[99, 33, 22]}
-      address={{ street: "123 Main Street", city: "New York" }}
-    ></UserCard>
-
-    <TaskCard ready={true}></TaskCard>
-
-    <Saludar />
-
-    <Posts></Posts>
-
-    {users.map((user, index) => {
-      //return
-
-      return (
-        <div key={index}>
-          <h1>{user.name + " " + (index + 1)}</h1>;
-          <img src={user.image} />
-        </div>
-      );
-    })}
+    <Counter></Counter>
   </>
 );
